8a37179f50fff4fbad3c3d6af1cfeceb57e5b578
Sam McCullough <samanuel.mc@gmail.com>
2019-12-05 19:36:55 -0800

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Sam on 2019-12-05.
+//  Copyright © 2019 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
